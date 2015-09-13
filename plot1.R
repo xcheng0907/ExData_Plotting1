42772e5d21d9e46dfdf635d8d3054aae6b26b6e4
XiangCheng <chengxiang.cn@gmail.com>
2015-09-12 23:31:31 -0400

    plot1.png is addded

--- plot1.R
+++ plot1.R
@@ -6,4 +6,6 @@ hpc = read.table("./data/small_data.txt", header=T, sep=";", na.strings = "?")
 
 
 # plotting 1
-
+png(filename='plot1.png',width=480,height=480, units='px')
+hist(hpc$Global_active_power, col="red", xlab="Global Active Power (kilowatts)", ylab="Frequency", main="Global Active Power")
+dev.off()
