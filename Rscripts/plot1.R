c6ee7f5d14c6689d57de43eaede658eb33381898
XiangCheng <chengxiang.cn@gmail.com>
2015-09-12 23:08:22 -0400

    remove data/household_power_consumption.txt

--- Rscripts/plot1.R
+++
@@ -1,8 +0,0 @@
-# exploratoryAnalysis Project 1
-setwd("/home/xcheng0907/GoogleDrive/Courses/DataScience_Coursera/ExploratoryAnalysis")
-
-hpc = read.table("household_power_consumption.txt", header=T, sep=";", na.strings = "?")
-hpc$Date = as.Date(hpc$Date, "%d/%m/%Y")
-d1 = as.Date("2007-02-01", "%Y-%m-%d")
-d2 = as.Date("2007-02-02", "%Y-%m-%d")
-h2 = hpc[hpc$Date ==d1 | hpc$Date==d2, ]
