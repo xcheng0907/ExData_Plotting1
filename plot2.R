fc26909e3d2e03fb824e102d544ee44ac1aa5586
XiangCheng <chengxiang.cn@gmail.com>
2015-09-13 10:42:29 -0400

    plot 2 is done!

---
+++ plot2.R
@@ -0,0 +1,12 @@
+# exploratoryAnalysis Project 1
+library(lubridate)
+setwd("/home/xcheng0907/GoogleDrive/Courses/DataScience_Coursera/ExploratoryAnalysis/ExData_Plotting1")
+
+# read the data 
+hpc = read.table("./data/small_data.txt", header=T, sep=";", na.strings = "?")
+hpc$date_time = ymd(hpc$Date) + hms(hpc$Time)
+
+# plotting 2
+png(filename='plot2.png',width=480,height=480, units='px')
+plot(hpc$date_time, hpc$Global_active_power, type='l', ylab="Global Active Power (kilowatts)", xlab="", main="Global Active Power")
+dev.off()
