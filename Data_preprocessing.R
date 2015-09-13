3a00fadc34bacdfea44d2dd7536e2ee511e1453b
XiangCheng <chengxiang.cn@gmail.com>
2015-09-12 23:08:22 -0400

    remove data/household_power_consumption.txt

---
+++ Data_preprocessing.R
@@ -0,0 +1,15 @@
+# exploratoryAnalysis Project 1
+setwd("/home/xcheng0907/GoogleDrive/Courses/DataScience_Coursera/ExploratoryAnalysis/ExData_Plotting1")
+
+# read the data 
+hpc = read.table("household_power_consumption.txt", header=T, sep=";", na.strings = "?")
+hpc$Date = as.Date(hpc$Date, "%d/%m/%Y")
+
+# subset the data to 2 days
+d1 = as.Date("2007-02-01", "%Y-%m-%d")
+d2 = as.Date("2007-02-02", "%Y-%m-%d")
+hpc = hpc[hpc$Date ==d1 | hpc$Date==d2, ]
+
+# write the small dataset to another file for future use 
+write.table(hpc, file="./data/small_data.txt", sep = ";")
+
--- plot1.R
+++ plot1.R
@@ -2,14 +2,8 @@
 setwd("/home/xcheng0907/GoogleDrive/Courses/DataScience_Coursera/ExploratoryAnalysis/ExData_Plotting1")
 
 # read the data 
-hpc = read.table("household_power_consumption.txt", header=T, sep=";", na.strings = "?")
-hpc$Date = as.Date(hpc$Date, "%d/%m/%Y")
+hpc = read.table("./data/small_data.txt", header=T, sep=";", na.strings = "?")
 
-# subset the data to 2 days
-d1 = as.Date("2007-02-01", "%Y-%m-%d")
-d2 = as.Date("2007-02-02", "%Y-%m-%d")
-hpc = hpc[hpc$Date ==d1 | hpc$Date==d2, ]
-write.table(hpc, file="small_data.txt")
 
 # plotting 1
 
